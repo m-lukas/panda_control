4ad248eda92eb7cb2551c42cb8995ebd5206ffc6
Lukas Mueller <mail@lukasmueller.de>
2025-08-06 17:23:34 +0200

    Change packaging notification and add task completion

--- src/programs.py
+++ src/programs.py
@@ -298,13 +298,13 @@ def move_to_packaging(move_group, *args, **kwargs):
         current_container_index += 1
         number_of_items_in_container = 0
 
+    # move to packaging common
+    move_to_pose(move_group, 0.4672196006673697, -1.6546232406214665, 1.6710899174338891, -2.5156881482141054, 0.38071471120251543, 2.900895444644822, -1.0297259617315397, 0.01507969293743372, 0.01507969293743372, 0.2)
+
     if current_container_index >= len(packaging_containers):
         notify_task_completed()
     else:
         notify_handover_finished()
-    
-    # move to packaging common
-    move_to_pose(move_group, 0.4672196006673697, -1.6546232406214665, 1.6710899174338891, -2.5156881482141054, 0.38071471120251543, 2.900895444644822, -1.0297259617315397, 0.01507969293743372, 0.01507969293743372, 0.2)
 
 
 def move_to_idle(move_group, *args, **kwargs):
